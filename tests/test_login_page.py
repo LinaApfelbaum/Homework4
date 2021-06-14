2b185107c4187ad3c0bc3965078bb39d5cfd3770
Lina Apfelbaum <mevakeret.heshbonot@gmail.com>
2021-06-14 21:55:19 +0500

    Added Allure annotations

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -1,6 +1,9 @@
+import allure
 from framework import get_element
 
 
+@allure.title("Search for elements on the Login page")
+@allure.description("Test checks that 5 elements are presented on the Login page")
 def test_login_page(browser, opencart_base_url, login_page):
     login_page.open(opencart_base_url)
     get_element(browser, login_page.EMAIL_FIELD)
